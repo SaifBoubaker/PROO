84814922e78a7943b2d2affcb980718ec9849516
SaifBoubaker <saif.boubaker.contact@gmail.com>
2022-10-10 11:37:11 +0100

    deploy

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/category", categryRoute);
 
 app.use(express.static(path.join(__dirname, "./", "client", "build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./", "client", "build", "index.html"));
 });
 
 app.use(notFound);
